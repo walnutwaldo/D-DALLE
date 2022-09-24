6963182b6d5f0608f5c3d98a4ee27071c1bc3bc1
Florian Juengermann <florianjuengermann@gmail.com>
2022-09-24 15:39:59 -0400

    added backend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+
+
+const prompt = (req, res) => {
+    console.log("prompt :", req.body);
+    const res_data = { "success": true, "data": req.body };
+    res.send(res_data);
+};
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(express.json())
+    .post('/prompt', prompt)
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
