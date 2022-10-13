38b1e10dc6a9695885846e47ed4b7224d211c62e
Walden Yan <waldenyan20@gmail.com>
2022-10-13 01:07:46 -0400

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer className='bg-slate-900 text-white h-12 grid content-center'>
-            <div className='text-md text-center'>Built for HackBoston 2022</div>
+            <div className='text-md text-center'>Built for HackBoston 2022 and KlayMakers 2022</div>
         </footer>
     );
 }
